5ba1c3ba60caaaabca55ef6bb4ad05eee3cea1ff
diepes7 <olivero.dieplinger@gmail.com>
2017-11-16 12:03:14 +0100

    Test
    Test

--- app/src/main/java/com/example/oliver/wandertagapp/MainActivity.java
+++ app/src/main/java/com/example/oliver/wandertagapp/MainActivity.java
@@ -1,13 +1,61 @@
 package com.example.oliver.wandertagapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private Button btnAnmeldenClick;
+    private boolean loginSuccessful;
+    private int benutzer_type;              //1 = Admin; 2 = Veranstalter; 3 = Benutzer
+    private Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //initVariables();
+        //initButtonClick();
+        setContentView(R.layout.activity_login);
     }
+    /*
+    private void initVariables() {
+        editTextEmail = (EditText) findViewById(R.id.email);
+        editTextPassword = (EditText) findViewById(R.id.password);
+        btnAnmeldenClick = (Button) findViewById(R.id.email_sign_in_button);
+
+    }
+
+    private void initButtonClick() {
+        btnAnmeldenClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkLoginData();
+            }
+        });
+    }
+
+    private void checkLoginData() {
+        if (loginSuccessful && benutzer_type == 1){
+            intent = new Intent(getApplicationContext(), AdminPage.class);
+            startActivity(intent);
+        }
+        if (loginSuccessful && benutzer_type == 2){
+            intent = new Intent(getApplicationContext(), VerwaltungPage.class);
+            startActivity(intent);
+        }
+        if (loginSuccessful && benutzer_type == 3){
+            intent = new Intent(getApplicationContext(), BenutzerPage.class);
+            startActivity(intent);
+        }
+    }
+*/
+
+
+
+
 }
---
+++ app/src/main/java/com/example/oliver/wandertagapp/VerwaltungPage.java
@@ -0,0 +1,13 @@
+package com.example.oliver.wandertagapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VerwaltungPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verwaltung_page);
+    }
+}
